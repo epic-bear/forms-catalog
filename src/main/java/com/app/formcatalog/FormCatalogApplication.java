d4c60f25a5d40bc9939c6fae834b00b812c9459b
Belov <ybelo@softserveinc.com>
2023-05-31 16:31:48 +0300

    forms-catalog

---
+++ src/main/java/com/app/formcatalog/FormCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.app.formcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormCatalogApplication.class, args);
+    }
+
+}
