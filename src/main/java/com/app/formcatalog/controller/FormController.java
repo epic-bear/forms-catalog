c832da6bc050fc76c49883a819cf294582914745
Belov <ybelo@softserveinc.com>
2023-06-04 10:07:30 +0300

    refactoring

--- src/main/java/com/app/formcatalog/controller/FormController.java
+++ src/main/java/com/app/formcatalog/controller/FormController.java
@@ -3,13 +3,8 @@ package com.app.formcatalog.controller;
 import com.app.formcatalog.domain.Form;
 import com.app.formcatalog.service.FormService;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Valid;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,8 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-import java.util.Set;
+import java.util.List;;
 
 @Controller
 @RequestMapping("/")
